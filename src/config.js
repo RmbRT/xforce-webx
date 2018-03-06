11d9cdf6b377e471b1eb44382e62e5f671ae4db2
RmbRT <steffen.rattay@gmail.com>
2018-03-06 14:42:18 +0100

    Added Config.js and started renewing options page.

---
+++ src/config.js
@@ -0,0 +1,156 @@
+/** The configuration data of the web extension.
+@param name:
+	String: The X-Force login name.
+@param password:
+	String: The X-Force login password.
+@param auto_check:
+	Boolean: Whether to check the threat level of visited sites automatically.
+@param threat_medium:
+	Number: The minimum threat level to treat as medium threat.
+@param threat_high:
+	Number: The minimum threat level to treat as high threat.
+@throws TypeError
+	If any of the arguments is not of the required type. */
+function Config(
+	name,
+	password,
+	auto_check,
+	threat_medium,
+	threat_high) {
+	// check and set name.
+	if(typeof(name) !== "string")
+		throw new TypeError("name must be a string.");
+	else
+		this._name = name;
+
+	// check and set password.
+	if(typeof(password) !== "string")
+		throw new TypeError("password must be a string.");
+	else
+		this._password = password;
+
+	// check and set auto_check.
+	if(typeof(auto_check) !== "boolean")
+		throw new TypeError("auto_check must be a boolean.");
+	else
+		this._auto_check = auto_check;
+
+	// check and set threat_medium.
+	if(typeof(threat_medium) !== "number")
+		throw new TypeEError("threat_medium must be a number.");
+	else
+		this._threat_medium = threat_medium;
+
+	// check and set threat_high.
+	if(typeof(threat_high) !== "number")
+		throw new TypeError("threat_high must be a number");
+	else
+		this._threat_high = threat_high;
+}
+
+/** Creates a config object from a json object. */
+Config.fromJSON = function(json) {
+	return new Config(
+		json.name,
+		json.password,
+		json.auto_check,
+		json.threat_medium,
+		json.threat_high);
+};
+
+/** Converts a config object into a json object. */
+Config.prototype.toJSON = function() {
+	return {
+		"name": this._name,
+		"password": this._password,
+		"auto_check": this._auto_check,
+		"threat_medium": this._threat_medium,
+		"threat_high": this._threat_high
+	};
+};
+
+/** The keys used to laod and save the config object. */
+Config.fields = ["name", "password", "auto_check", "threat_medium", "threat_high"];
+
+/** The default config values.
+	This is used to create initial config values. */
+Config.defaultJSON = {
+	"name": "",
+	"password": "",
+	"auto_check": false,
+	"threat_medium": 4,
+	"threat_high": 7
+};
+
+/** Loads the configuration.
+@param then:
+	Function(Config): Called when the config is successfully loaded.
+@param fail:
+	Function(Error): Called when the config could not be loaded.
+@throws TypeError
+	If the arguments were not of the right type. */
+Config.load = function(then, fail) {
+	// check argument types.
+	if(typeof(then) !== "function")
+		throw new TypeError("then must be a function taking 1 argument.");
+	else if(typeof(fail) !== "function")
+		throw new TypeError("fail must be a function taking 1 argument.");
+
+	// attempt to load the configuration object.
+	browser.storage.local.get(Config.fields).then(
+		((then) => { return (c) => {
+			// replace unset values with default values.
+			for(var i = 0; i < Config.fields.length; i++)
+				if(!(Config.fields[i] in c))
+					c[Config.fields[i]] = Config.defaultJSON[Config.fields[i]];
+			// return the config object.
+			then(Config.fromJSON(c));
+		};
+	})(then)).catch(fail);
+};
+
+/** Saves a configuration object.
+@param then:
+	Function(): Called when the config is successfully saved.
+@param fail:
+	Function(Error): Called when the config could not be saved.
+@throws TypeError
+	If the arguments were not of the right type. */
+Config.prototype.save = function(then, fail) {
+	// check argument types.
+	if(typeof(then) !== "function" || then.arguments !== 1)
+		throw new TypeError("then must be a function taking 1 argument.");
+	else if(typeof(fail) !== "function" || fail.arguments !== 1)
+		throw new TypeError("fail must be a function taking 1 argument.");
+
+	// attempt to save the configuration object.
+	browser.storage.local.set(this.toJSON()).then(then).catch(fail);
+};
+
+/** Returns the X-Force login name. */
+Config.prototype.name = function() { return this._name; };
+
+/** Returns the X-Force login password. */
+Config.prototype.password = function() { return this._password; };
+
+/** Returns whether every visited site should be checked automatically. */
+Config.prototype.autoCheck = function() { return this._auto_check; };
+
+/** Returns the minimum threat level to treat as medium threat. */
+Config.prototype.threatMedium = function() { return this._threat_medium; };
+
+/** Returns the minimum threat level to treat as high threat. */
+Config.prototype.threatHigh = function() { return this._threat_high; };
+
+/** Classifies a threat level.
+	The classification depends on the `threatMedium` and `threatHigh` properties.
+@return
+	"low", "medium", or "high". */
+Config.prototype.threatLevel = function(level) {
+	if(level >= this.threatHigh())
+		return "high";
+	else if(level >= this.threatMedium())
+		return "medium";
+	else
+		return "low";
+};
--- src/options.js
+++ src/options.js
@@ -1,26 +1,90 @@
-const setting_names = ["username", "password"];
+function displayError(message) {
+	document.getElementById("error").innerText += "Error: " + message + "\n";
+	document.getElementById("success").innerText = "";
+}
+
+function displaySuccess(message) {
+	document.getElementById("success").innerText += "Success: " + message + "\n";
+	document.getElementById("error").innerText = "";
+}
+
+function clearMessage() {
+	document.getElementById("error").innerText = "";
+	document.getElementById("success").innerText = "";
+}
 
 /** Loads the settings and writes them into the form. */
 function loadSettings() {
-	browser.storage.local.get(setting_names).then((s) => {
-		for(var i in s)
-			document.getElementById(i).value = s[i];
-	}).catch((e) => {
-		document.getElementById("error").innerText = "Error: Exception when loading: " + e;
+	Config.load((c) => {
+		try {
+			document.getElementById("name").value = c.name().toString();
+			document.getElementById("password").value = c.password().toString();
+			document.getElementById("auto-check").checked = !!c.autoCheck();
+			document.getElementById("threat-medium").value = c.threatMedium().toString();
+			document.getElementById("threat-high").value = c.threatHigh().toString();
+		} catch(e) {
+			displayError("Internal: " + e);
+		}
+	}, (e) => {
+		displayError("Exception when loading settings: " + e);
 	});
 }
 
 // Execute this code when the HTML of the options page is fully loaded.
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", ((allValid) => { return function() {
+	// set up validation listeners.
+	document.getElementById("name").addEventListener("input", (o) => {
+		if(o.value === "") {
+			allValid &= ~1;
+			document.getElementById("invalid-name").classList.add("invalid");
+		} else {
+			allValid |= 1;
+			document.getElementById("invalid-name").classList.remove("invalid");
+		}
+	});
+	document.getElementById("password").addEventListener("change", () => {
+	});
+
+
 	// fill the previous settings into the form.
 	loadSettings();
 
-	// on clicking the save button, save the form.
-	document.getElementById("form").addEventListener("submit", () => {
-		var settings = {};
-		for(var i = 0; i < setting_names.length; i++)
-			settings[setting_names[i]] = document.getElementById(setting_names[i]).value;
 
-		browser.storage.local.set(settings);
+
+	// on clicking the save button, save the form.
+	document.getElementById("save").addEventListener("click", () => {
+		// clear the status message.
+		clearMessage();
+		displaySuccess("kefekf");
+
+		// read the settings from the form.
+		var settings = {
+			"name": document.getElementById("name").value,
+			"password": document.getElementById("password").value,
+			"auto_check": document.getElementById("auto-check").checked,
+			"threat_medium": Number.parseInt(document.getElementById("threat-medium").value),
+			"threat_high": Number.parseInt(document.getElementById("threat-high").value)
+		};
+
+		// validate the input.
+		if(settings.name === "" || settings.password === "")
+			displayError("Name and password are required.");
+		else if(Number.isNaN(settings.threat_medium))
+			displayError("Medium threat level must be a number.");
+		else if(Number.isNaN(settings.threat_high))
+			displayError("High threat level must be a number.");
+		else if(settings.threat_medium >= settings.threat_high)
+			displayError("Medium threat level must be < high threat level.");
+		else
+		{
+			// save the settings.
+			try {
+				Config.fromJSON(settings).save(
+					( ) => { displaySuccess("Saved settings."); },
+					(e) => { displayError("Failed to save the settings."); });
+			} catch(e) {
+				displayError("Internal: " + e);
+			}
+		}
 	});
-});
+}; })(0));
