8c53bd92c5fa6dd08ef3ecd98f4cb2e1d0825f88
RmbRT <steffen.rattay@gmail.com>
2018-03-21 16:23:37 +0100

    Improved report style.

--- src/response.js
+++ src/response.js
@@ -1,8 +1,15 @@
-Requirement.need("response.js", []);
+Requirement.need("response.js", ["config.js"]);
 const urlDomain = ((a)=> { return function(url) {
 	a.href = url;
 	return a.hostname;
 };})(document.createElement("a"));
+
+
+var config;
+Config.load(
+	c => { config = c; },
+	() => alert("Failed to load config."));
+Config.listenForUpdates(c => { config = c; });
 
 /** Adds a report to all links to a url.
 @param url:
@@ -21,53 +28,19 @@ const addReport = ((globalReport) => { return function(report, request) {
 		{
 			// create the report html element.
 			var e = document.createElement("span");
-			
 
-			// add the report style.
-			
-
-			/*var score = document.createElement("div");
-			
-			var hScore = document.createElement("div");
-			hScore.innerText = "Score";
-			hScore.classList.add("center");
-			score.appendChild(hScore);
-
-			var risk = document.createElement("div");
-			risk.innerText = report.score;
-			risk.classList.add("center");
-			score.appendChild(risk);
-			switch(true){
-				case report.score < 4: score.classList.add("low-risk");
-				break;
-				case report.score> 4 && report.risk < 7: score.classList.add("medium-risk");
-				break;
-				default: score.classList.add("high-risk");
-			}
-			e.appendChild(score);
-
-			var hCats = document.createElement("div");
-			hCats.classList.add("domain");
-			hCats.innerText = "Domain: " + hostname;
-			e.appendChild(hCats);
-
-			var tCats = document.createElement("p");
-			tCats.classList.add("cats");*/
 			var catString = "";
-			
 			for(var cat in report.cats)
 				catString += `<li class="LI_53">${cat}</li>`;
-			
-		
-			
+
 			e.innerHTML =
-`<div class="DIV_1">
-	<div class="DIV_2" class="low-risk">
+`<div class="root">
+	<div class="risk-box ${config.threatLevel(report.score)}-risk">
 		<div class="DIV_3">Risk</div>
 		<div class="DIV_4">${report.score}</div>
 	</div>
 	<div class="DIV_5">
-		<div class="DIV_6">			
+		<div class="DIV_6">
 			<h2 class="H2_7">
 				<div class="DIV_13">
 					<button type="button" class="BUTTON_14">Add to Collection</button>
@@ -97,7 +70,7 @@ const addReport = ((globalReport) => { return function(report, request) {
 				</tbody>
 			</table>
 		</div>
-</div>
+	</div>
 </div>`;
 			e.querySelector("button").addEventListener("click", (url => { return function() {
 				Messaging.sendToBackground("Collection.addReport", url).then((r)=>{
